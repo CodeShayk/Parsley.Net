b71945cdc3ba038390571b2ead542b88ac1a2c93
Code Ninja <najaf.shaikh@gmail.com>
2025-05-08 22:43:18 +0100

    Delete src/parsley directory

--- src/parsley/IFileLine.cs
+++
@@ -1,8 +0,0 @@
-namespace parsley
-{
-    public interface IFileLine
-    {
-        public int Index { get; set; }
-        public IList<string> Errors { get; set; }
-    }
-}
--- src/parsley/IParser.cs
+++
@@ -1,9 +0,0 @@
-namespace parsley
-{
-    public interface IParser
-    {
-        public T[] Parse<T>(string filepath) where T : IFileLine, new();
-
-        public T[] Parse<T>(string[] lines) where T : IFileLine, new();
-    }
-}
